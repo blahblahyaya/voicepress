d40fffce82bf090ac042f4d1a7465326f65ae807
Andrew Roberts <andy@behladesign.com>
2017-09-15 15:50:14 -0700

    add skills cpt

---
+++ voicepress.php
@@ -0,0 +1,15 @@
+/**
+ * Register a skill post type, with REST API support
+ *
+ * Based on example at: https://codex.wordpress.org/Function_Reference/register_post_type
+ */
+add_action( 'init', 'my_skill_cpt' );
+function my_skill_cpt() {
+    $args = array(
+      'public'       => true,
+      'show_in_rest' => true,
+      'rest_base'    => 'skills',
+      'label'        => 'Skills'
+    );
+    register_post_type( 'skill', $args );
+}
